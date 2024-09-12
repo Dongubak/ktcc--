c8c48e923153f3c3367951ed4672c0f31604dd83
Junius B. Baelish <aqwer145662@gmail.com>
2024-09-12 10:25:25 +0900

    2024.09.12 bree
    math.h

--- JBB/iostreamtest.cpp
+++ JBB/iostreamtest.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+#include <math.h> 
+int main()
+{
+using namespace std;
+double area;
+cout << "Enter the floor area, in square feet, of your home: ";
+cin >> area;
+double side;
+side = sqrt(area);
+cout << "That’s the equivalent of a square " << side
+<< " feet to the side." << endl;
+cout << "How fascinating!" << endl;
+return 0;
+}
